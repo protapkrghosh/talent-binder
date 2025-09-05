a93a68128fbc7f1cc7965306708d319f75c23e9d
protapkrghosh <protapg518@gmail.com>
2025-09-06 05:07:56 +0600

    added mongodb in server side

--- talent-binder-server/index.js
+++ talent-binder-server/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,6 +8,27 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(process.env.TALENT_BINDER_URI, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   },
+});
+
+async function run() {
+   try {
+      await client.connect();
+      await client.db("admin").command({ ping: 1 });
+      console.log(
+         "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+   } finally {
+      // await client.close();
+   }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
    res.send("Talent Binder is running");
